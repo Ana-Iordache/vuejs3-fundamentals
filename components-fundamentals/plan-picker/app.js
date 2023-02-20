d6262c2403182f4697fda4bef19bd9fd6261ce7b
ana_iordache <ana.iordache@acrom.ro>
2023-02-20 16:44:19 +0200

    global vs local component

--- components-fundamentals/plan-picker/app.js
+++ components-fundamentals/plan-picker/app.js
@@ -1,15 +1,5 @@
-Vue.createApp({
-    
-})
-.component('plan-picker',{
-    template:'#plan-picker-template',
-    data(){
-        return{
-            plans:['The Single', 'The Curious', 'The Addict']
-        }
-    }
-})
-.component('plan',{
+// local component
+let PlanComponent = {
     template:'#plan-template',
     // props: ['name']
     props: {
@@ -17,5 +7,25 @@ Vue.createApp({
         price: Number
     }
     // in this way we wil get warning/error if we pass an incorrect type
+}
+
+const app = Vue.createApp({
+    
+})
+// global registration -> makes the components globally available in the app
+// the problem is, if you are using a build system like Vite, globbally reg means even if you stop using a component, it will still be included in the final bundle
+// => unnecessarily increases the amount of Javascript the users have to download
+
+// so, we are creating a variable which will be the childer of this component
+// and here we will use the components option to register the plan locally
+// now we cannot use the plan component outside the plan-picker's template
+.component('plan-picker',{
+    components:{plan: PlanComponent},
+    template:'#plan-picker-template',
+    data(){
+        return{
+            plans:['The Single', 'The Curious', 'The Addict']
+        }
+    }
 })
 .mount('#app')
