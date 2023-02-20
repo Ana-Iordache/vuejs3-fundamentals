68934248bdfc498f36800f637bd4209cf4362a77
ana_iordache <ana.iordache@acrom.ro>
2023-02-20 16:31:27 +0200

    nested components

--- components-fundamentals/plan-picker/app.js
+++ components-fundamentals/plan-picker/app.js
@@ -1,4 +1,8 @@
 Vue.createApp({
+    
+})
+.component('plan-picker',{
+    template:'#plan-picker-template',
     data(){
         return{
             plans:['The Single', 'The Curious', 'The Addict']
