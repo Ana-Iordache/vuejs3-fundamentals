ea42d8b1211b692638da624211d98a44316496f1
ana_iordache <ana.iordache@acrom.ro>
2023-02-20 16:25:48 +0200

    reusable components with props

--- components-fundamentals/plan-picker/app.js
+++ components-fundamentals/plan-picker/app.js
@@ -1,7 +1,17 @@
 Vue.createApp({
-
+    data(){
+        return{
+            plans:['The Single', 'The Curious', 'The Addict']
+        }
+    }
 })
 .component('plan',{
-    template:'#plan-template'
+    template:'#plan-template',
+    // props: ['name']
+    props: {
+        name: {type:String, default: 'The Single', required:true},
+        price: Number
+    }
+    // in this way we wil get warning/error if we pass an incorrect type
 })
 .mount('#app')
