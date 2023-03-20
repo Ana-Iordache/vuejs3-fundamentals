0333d8fb973289f8f211b55aeed2a35f5ba4a3c2
ana_iordache <ana.iordache@acrom.ro>
2023-03-20 15:07:40 +0200

    history mode

--- vue-router4/travel-app/src/router/index.js
+++ vue-router4/travel-app/src/router/index.js
@@ -44,6 +44,14 @@ const routes = [
 
 // this creates the router instance for our application
 const router = createRouter({
+  // this is created for us because we chose to use web history during the setup with the Vue CLI
+  // if we didn't choose that, we'd have createWebHashHistory
+  // with this, when we click on a browser on an image, the url will be "localhost:8001/countryName" <=> html5 mode
+  // -> this means that under the hood, vue router is using the html5 history API
+  // to produce urls that look normal;
+  // if we used createWebHashHistory, the url would be "localhost:8001/#/countryName" <=> hash mode
+  // the purpose is to imitate a file path whithout having to setup any special server configs
+
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
